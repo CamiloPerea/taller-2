c03577fd4aa8b9382d2cd45942f3d1564940488d
CamiloPerea <158522172+CamiloPerea@users.noreply.github.com>
2024-03-06 21:15:45 -0500

    Add files via upload
    Camilo Perea

---
+++ eje1 (1).py
@@ -0,0 +1,12 @@
+#Elaborar un programa en python que permita gestionar una tienda de videojuegos donde se prestan y se devuelven los mismos.
+cons_existenciasInt = 5
+var_cantidadInt = int(input('Ingrese la cantidad de videojuegos a prestar -> '))
+if var_cantidadInt<=0:
+    print('La cantidad de videojuegos debe ser mayor a 0')
+else:
+    if var_cantidadInt <= cons_existenciasInt:
+        print('Tu prestamo ha sido aprobado')
+        print('Existencias actuales: ', (cons_existenciasInt-var_cantidadInt))
+        enter = input('Presione <ENTER> para continuar')
+    else:
+        print('No tenemos esa cantidad de videojuegos disponible')
